c936bf91ecdbf4303bfaa7bd92e53317a4be6d68
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-27 10:20:02 -0600

    parent views done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 var TargetPractice = angular.module('TargetPractice', ['ui.router']);
 
-TargetPractice.config
+TargetPractice.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('parent', {
+      url: '/',
+      views: {
+        "": {
+          template: "<p>Targeting unnamed view from parent state</p>"
+        },
+        "named-parent": {
+          template: "<p>Targeting named view from parent state</p>"
+        },
+        "main-header": {
+          template: "<p>Targeting header from parent state</p>"
+        }
+      }
+    });
+
+});
