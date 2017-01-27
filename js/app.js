96d88d4e0a6b9f48defc9ac3b375eb946c624cd2
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-27 11:40:50 -0600

    finished assignment

--- js/app.js
+++ js/app.js
@@ -38,6 +38,29 @@ TargetPractice.config(function($stateProvider, $urlRouterProvider) {
           template: "<p>Targeting header from child state</p>"
         }
       }
+    })
+    .state('parent.child.grandchild', {
+      url: '/grandchild',
+      views: {
+        "@parent.child": {
+          templateUrl: "/js/templates/grandchild.html"
+        },
+        "@parent.child.grandchild": {
+          template: "<p>Targeting unnamed grandchild view from grandchild state</p>"
+        },
+        "named-grandchild@parent.child.grandchild": {
+          template: "<p>Targeting named grandchild view from grandchild state</p>"
+        },
+        "named-child@parent.child": {
+          template: "<p>Targeting named child view from grandchild state</p>"
+        },
+        "named-parent@": {
+          template: "<p>Targeting named parent view from grandchild state</p>"
+        },
+        "main-header@": {
+          template: "<p>Targeting header from grandchild state</p>"
+        }
+      }
     });
 
 });
